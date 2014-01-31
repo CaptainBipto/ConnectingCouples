d27e506ad12b6c68364652009777be8d4ebabc62
Norm <Norm@Weitzel@ebDesigns.com>
2014-01-31 10:58:00 -0800

    finished / refactored flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,17 @@ module ApplicationHelper
   end
  end
 
+
+ def flash_class(type)
+   case type
+     when :alert
+       "alert-danger"
+     when :notice
+       "alert-success"
+     else
+       ""
+   end    
+ end
+ 
+ 
 end
