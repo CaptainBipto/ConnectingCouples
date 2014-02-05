f32f5cd545b3f18537a8c7a60cf732b55faaba59
Norm <Norm@Weitzel@ebDesigns.com>
2014-02-05 11:48:49 -0800

    added status controller and model tests

--- app/models/status.rb
+++ app/models/status.rb
@@ -3,4 +3,8 @@ class Status < ActiveRecord::Base
   
   belongs_to :user
   
+  validates :content, presence: true,
+                      length: { minimum: 2 }
+  
+  validates :user_id, presence: true
 end
--- test/models/status_test.rb
+++ test/models/status_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class StatusTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "that a status requires content" do
+    status = Status.new
+    assert !status.save
+    assert !status.errors[:content].empty?
+  end
+
+  test "that a status contains at least 2 characters" do
+    status = Status.new
+    status.content = "h"
+    assert !status.save
+    assert !status.errors[:content].empty?
+  end
+  
+  test "that a status has a user id (is posted by a registered user)" do
+    status = Status.new
+    status.content = "Hello"
+    assert !status.save
+    assert !status.errors[:user_id].empty?
+  end
+
 end
