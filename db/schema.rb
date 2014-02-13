f146c161a77ab39b7637547cebdd5cda7c43a03c
Norm <Norm@Weitzel@ebDesigns.com>
2014-02-12 17:44:07 -0800

    created UserFriendship Model, ran migration.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140109172629) do
+ActiveRecord::Schema.define(version: 20140213012650) do
 
   create_table "quizzes", force: true do |t|
     t.integer  "user_id"
@@ -32,6 +32,15 @@ ActiveRecord::Schema.define(version: 20140109172629) do
   end
 
   add_index "statuses", ["user_id"], name: "index_statuses_on_user_id"
+
+  create_table "user_friendships", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "friend_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "user_friendships", ["user_id", "friend_id"], name: "index_user_friendships_on_user_id_and_friend_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
